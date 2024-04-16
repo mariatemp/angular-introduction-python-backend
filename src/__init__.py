23391ee7f6e78a34d05b3aab00cd7d78b32d07e9
Maria Temponera <marakitempo@yahoo.gr>
2024-04-16 20:43:49 +0300

    user-registration

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@ jwt = JWTManager(app)
 app.config["JWT_SECRET_KEY"] = "super secret and difficult to guess key"
 
 connect(
-    host="mongodb+srv://coding-factory:ufTXpw9bAEejrXfb@cluster0.okry00y.mongodb.net",
+    host="mongodb+srv://cf5user:LcxZLkA8F0mBpt29@cluster0.9svszzw.mongodb.net/",
     db="Coding-Factory",
     alias="coding-factory",
 )
